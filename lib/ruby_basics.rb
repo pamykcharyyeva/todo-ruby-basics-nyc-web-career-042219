49b0e8392347b5b154060b2c87636388dd3cf306
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-01 23:51:03 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return (num1/num2)
 end
 
-def assign_variable(value)
+def assign_variable(name)
+  return "#{name}"
+end
+
+def argue(name)
+  return "#{name}"
+end
+
+def greeting(greeting, name)
   
 end
 
-def argue
-  
+def return_a_value(value="Nice")
+return "#{value}"
 end
 
-def greeting
-
+def last_evaluated_value(phrase="expert")
+return phrase
 end
 
-def return_a_value
-
-end
-
-def last_evaluated_value
-
-end
-
-def pizza_party
+def pizza_party(name="cheese")
+  return name
 
 end
